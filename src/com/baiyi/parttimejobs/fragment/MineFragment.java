c3902ca8fdd83f8610cc69ff6f9c0d393c89a628
gangxie <527332470@qq.com>
2015-03-22 12:26:14 +0800

    22222

--- src/com/baiyi/parttimejobs/fragment/MineFragment.java
+++ src/com/baiyi/parttimejobs/fragment/MineFragment.java
@@ -1,55 +1,62 @@
 package com.baiyi.parttimejobs.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.baiyi.parttimejobs.R;
+import com.baiyi.parttimejobs.activity.mineMyCollectionActivity;
+import com.baiyi.parttimejobs.activity.mine_personalinfoActivity;
 
-public class MineFragment extends Fragment/*implements View.OnClickListener*/{
-/*	private View view1;
-	private View view2;
-	private View view3;
-	private View view4;
-	private View view5;
-	private View view6;
-	private View view7;
-    private View[] views={view1,view2,view3,view4,view5,view6,view7};
-	private View view;*/
-	@Override
+public class MineFragment extends Fragment {
+   
+	private View personalInfo;
+	private View mycollection;
+	View view;
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		    View view = inflater.inflate(R.layout.activity_mine, null);
-	       
+		   view = inflater.inflate(R.layout.activity_mine, null);
+	     initView();
 		return view;
 	}
- /*  public void initView(){
-	view1=view.findViewById(R.id.button1);
-   	view2=view.findViewById(R.id.button2);
-   	view3=view.findViewById(R.id.button3);
-   	view4=view.findViewById(R.id.button4);
-   	view5=view.findViewById(R.id.button5);
-   	view6=view.findViewById(R.id.button6);
-   	view7=view.findViewById(R.id.button7);
-   	
-   	for(int i=0;i<7;i++){
-   		views[i].setTag(i);
-   	}
-   	
-   	
-   }
-@Override
-public void onClick(View arg0) {
-	// TODO Auto-generated method stub
-	switch(Integer.parseInt((String) arg0.getTag())){
+   
 	
+	public void initView(){
+		
+		mycollection=(View)view.findViewById(R.id.userinfo_mine_button2);
+		personalInfo=(View)view.findViewById(R.id.userinfo_mine_button1);
+		mycollection.setOnClickListener(new OnClickListener(){
+		       
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						Intent intent=new Intent(getActivity(),mineMyCollectionActivity.class);
+						getActivity().startActivity(intent);
+					}
+					
+				});
+		
+		personalInfo.setOnClickListener(new OnClickListener(){
+       
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(getActivity(),mine_personalinfoActivity.class);
+				getActivity().startActivity(intent);
+			}
+			
+		});
 	}
-	
-	
-}
-   */
+
+
+ 
   
 }
